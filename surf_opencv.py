e79f6340345d5ce673168346f77782a0d56b62b0
Vivek Wisdom <ervivekwisdom@gmail.com>
2017-08-30 14:10:07 +0530

    SURF Sample Complete

--- surf_opencv.py
+++ surf_opencv.py
@@ -11,20 +11,19 @@ import os
 import sys
 
 def main():
-    img = cv2.imread('sample.jpg', 0)
-
-    surf = cv2.xfeatures2d.SURF_create(400)
-    kp = surf.detect(img,None)
-    print(len(kp))
-    img2 = cv2.drawKeypoints(img,kp,None,(255,0,0),4)
+    img = cv2.imread('sample.jpg')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    surf = cv2.xfeatures2d.SURF_create(5000)
+    kp, des = surf.detectAndCompute(gray,None)
+    img2 = cv2.drawKeypoints(gray,kp,img,(255,0,0),4)
 
     # Save the Annotated Image to output directory
-    path = 'C:/Users/VIVTRIPATHI/Desktop/Projects/OpenCVLearn/output/'
+    path = os.getcwd() + '/output/'
     file_name = os.path.basename(sys.argv[0])
 
-    cv2.imwrite(str(path)+'{0}.jpg'.format(file_name),img)
+    cv2.imwrite(str(path)+'{0}.jpg'.format(file_name),img2)
 
-    plt.imshow(img), plt.show()
+    plt.imshow(img2), plt.show()
 
 if __name__ == '__main__':
     main()
